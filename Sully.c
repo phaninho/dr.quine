fa2d160954dd2476dd53b86659e5432cedd895bf
phaninho <phaninho@live.fr>
2018-01-15 00:44:36 +0100

    improve sully 1 result to much

--- Sully.c
+++ Sully.c
@@ -7,20 +7,20 @@ int main()
 char dc = '"';
 char n = 10;
 int i = 5;
-if (i <= 0)
+if (i < 0)
 return 0;
 char file_name[256];
 sprintf(file_name, "Sully_%d.c", i);
 i--;
 FILE *file = fopen(file_name, "w");
-char *buff = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$cint main()%1$c{%1$cchar dc = '%2$c'; %1$cchar n = 10;%1$cint i = %3$d;%1$cif (i <= 0)%1$creturn 0; %1$cchar file_name[16]; %1$csprintf(file_name, %2$cSully_%%d.c%2$c, i);%1$ci--; %1$cFILE *file = fopen(file_name, %2$cw%2$c);%1$cchar *buff=%2$c%4$s%2$c;%1$cfprintf(file, buff, n,dc, i, buff);%1$cfclose(file);%1$cchar buff2[500];%1$csprintf(buff2, %2$cclang -Wall -Wextra -Werror -o %%.*s %%s%2$c, (int)strlen(file_name)-2, file_name,file_name);%1$csystem(buff2);%1$csprintf(buff2, %2$c./%%.*s%2$c, (int)strlen(file_name), file_name);%1$csystem(buff2);%1$c%1$creturn 0;%1$c}";
+char *buff = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$cint main()%1$c{%1$cchar dc = '%2$c';%1$cchar n = 10;%1$cint i = %3$d;%1$cif (i < 0)%1$creturn 0;%1$cchar file_name[256];%1$csprintf(file_name, %2$cSully_%%d.c%2$c, i);%1$ci--;%1$cFILE *file = fopen(file_name, %2$cw%2$c);%1$cchar *buff = %2$c%4$s%2$c;%1$cfprintf(file, buff, n,dc, i, buff);%1$cfclose(file);%1$cchar buff2[500];%1$csprintf(buff2, %2$cclang -Wall -Wextra -Werror -o %%.*s %%s%2$c, (int)strlen(file_name)-2, file_name, file_name);%1$csystem(buff2);%1$csprintf(buff2, %2$c./%%.*s%2$c, (int)strlen(file_name)-2, file_name);%1$csystem(buff2);%1$c%1$creturn 0;%1$c}%1$c";
 fprintf(file, buff, n,dc, i, buff);
 fclose(file);
 char buff2[500];
 sprintf(buff2, "clang -Wall -Wextra -Werror -o %.*s %s", (int)strlen(file_name)-2, file_name, file_name);
 system(buff2);
-sprintf(buff2, "./%.*s", (int)strlen(file_name), file_name);
+sprintf(buff2, "./%.*s", (int)strlen(file_name)-2, file_name);
 system(buff2);
 
-  return 0;
+return 0;
 }
