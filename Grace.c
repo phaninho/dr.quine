5da24abca0577ba4a02cca189d28f78d77508068
Stephane MARTINS <stmartin@e1r9p20.42.fr>
2018-01-15 17:22:59 +0100

    delete usseless files

--- Grace.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#define Q(x)char*s=#x;x
-#define MULT(x, y) (x)*(y)
-#define hello(a) printf(#a)
-/*jaime les commentaires*/
-Q(int main(){FILE* fichier = NULL;fichier = fopen("Grace_kid.c", "w");fputs("#include <stdio.h>\n#define Q(x)char*s=#x;x\n#define MULT(x, y) (x)*(y)\n#define hello(a) printf(#a)\n/*jaime les commentaires*/\nQ(", fichier);fputs(s, fichier);fputs(")\n", fichier);})
